f8ba9395b3aa12bf77007f0dd1b51cd434e92371
Juan <jpichardo369@gmail.com>
2022-03-05 20:26:56 -0500

    Initial commit

---
+++ docusaurus.config.js
@@ -0,0 +1,116 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'applejuice Guide',
+  tagline: 'Guide for hosting on applejuice.bar',
+  url: 'https://guide.applejuice.bar',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/logo.png',
+  organizationName: 'applejuice-server', // Usually your GitHub org/user name.
+  projectName: 'applejuice', // Usually your repo name.
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          breadcrumbs: false,
+          showLastUpdateTime: true,
+          showLastUpdateAuthor: true
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  plugins: [
+    [
+      '@docusaurus/plugin-pwa',
+      {
+        debug: true,
+        offlineModeActivationStrategies: [
+          'appInstalled',
+          'standalone',
+          'queryString',
+        ],
+        pwaHead: [
+          {
+            tagName: 'link',
+            rel: 'icon',
+            href: '/img/logo.png',
+          },
+          {
+            tagName: 'link',
+            rel: 'manifest',
+            href: '/manifest.json', // your PWA manifest
+          },
+          {
+            tagName: 'meta',
+            name: 'theme-color',
+            content: 'rgb(255, 28, 28)',
+          },
+        ],
+      },
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: 'applejuice.bar',
+        logo: {
+          alt: 'applejuice logo',
+          src: 'img/logo.png',
+        },
+        items: [
+          {
+            type: 'doc',
+            docId: 'intro',
+            position: 'left',
+            label: 'Guide',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'More',
+            items: [
+              {
+                label: 'Discord',
+                href: 'https://discord.com/invite/X7ujJThANP',
+              },
+              {
+                label: 'Twitter',
+                href: 'https://twitter.com/applejuiceuhc',
+              },
+              {
+                label: 'NameMC',
+                href: 'https://namemc.com/server/applejuice.bar'
+              }
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()}, applejuice.bar | Built with Docusaurus.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
